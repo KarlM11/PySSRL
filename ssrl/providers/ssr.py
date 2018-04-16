fa336188d43720f3e140b6594ca449682a55f8df
Aspirin Geyer <aspirin.geyer@yandex.com>
2018-04-16 23:48:33 +0800

    Update data type.
    - Update tests.

--- ssrl/providers/ssr.py
+++ ssrl/providers/ssr.py
@@ -1,34 +1,85 @@
 # -*- coding:utf-8 -*-
 import base64
+import six
 from typing import Dict
+from urllib.parse import parse_qsl
 from ssrl.functional import default_encoding
 from .base import BaseProvider
 
 
 class SSRProvider(BaseProvider):
+
+    _scheme = 'ssr://'
+    _template = '{}'
+
+    # Defines param fields.
+    # Neither of them is required.
+    # fields -> name, is_encode, type
+    _param_fields = (
+        ('group', True, str),
+        ('obfs_param', True, str),
+        ('protoparam', True, str),
+        ('remarks', True, str),
+        ('udpport', False, int),
+        ('uot', False, int)
+    )
     
     @staticmethod
     def dumps(conf: Dict) -> str:
         pass
 
-    @staticmethod
-    def loads(link: str) -> dict:
-        link_ = link.encode(default_encoding)  # Convert to bytes.
-        pass
+    @classmethod
+    def loads(cls, link: str) -> dict:
+        if not link.lower().startswith(cls._scheme):
+            raise ValueError('Bad link.')
+
+        body = link[len(cls._scheme):]
+        body = cls.b64decode(body)
+        base, extra = body.split('/?')  # Split body and params.
+
+        host, port, proto, method, obfs, pass_en = base.split(':')
+        params = dict(parse_qsl(extra))  # Cast parsed params to dict.
+        passwd = cls.b64decode(pass_en)
+
+        conf = {
+            'server': host,
+            'server_port': port,
+            'method': method,
+            'password': passwd,
+            'protocol': proto,
+            'obfs': obfs
+        }
+
+        parsed_params = dict()
+        for k, e, t in cls._param_fields:
+            v = params.get(k, None)
+            if not v:
+                parsed_params[k] = "" if t is str else None
+                continue
+
+            if e:
+                v = cls.b64decode(v)
+
+            parsed_params[k] = t(v)
+            
+        conf['params'] = parsed_params
+        return conf
 
     @staticmethod
-    def b64encode(input_: bytes) -> bytes:
-        _encoded = base64.urlsafe_b64encode(input_)
-        _encoded = _encoded.replace(b'==', b'')  # Remove padding
+    def b64encode(input_: str) -> str:
+        input_ = input_.encode(default_encoding)
+        _encoded = base64.urlsafe_b64encode(input_) \
+                         .decode(default_encoding)
+
+        _encoded = _encoded.replace('==', '')  # Remove padding
         return _encoded
-
     @staticmethod
-    def b64decode(input_: bytes) -> bytes:
+    def b64decode(input_: str) -> str:
+        input_ = input_.encode(default_encoding)
         length = len(input_)
         pad_len = length % 4
-        if pad_len == 3:
-            raise ValueError("Bad input encoded string.")
 
+        # Base64 library accepts extra paddings.
         pad = b'=' * pad_len
         input_ += pad
-        return base64.urlsafe_b64decode(input_)
+        return base64.urlsafe_b64decode(input_).decode(default_encoding)
--- tests/test_ssr_provider.py
+++ tests/test_ssr_provider.py
@@ -1,28 +1,50 @@
 # -*- coding:utf-8 -*-
 from unittest import TestCase
+from ssrl.functional import default_encoding
 from ssrl.providers.ssr import SSRProvider
 
 
 class TestSSRProvider(TestCase):
 
     def test_b64_encode(self):
-        input_ = b'breakwa11.moe'
-        expected = b'YnJlYWt3YTExLm1vZQ'
+        input_ = 'breakwa11.moe'
+        expected = 'YnJlYWt3YTExLm1vZQ'
         result = SSRProvider.b64encode(input_)
 
         self.assertEqual(result, expected)
 
     def test_b64_decode(self):
         import base64
-        input_ = b'breakwa11.moe'
-        standard = base64.urlsafe_b64encode(input_)  # Standard with paddings.
-        no_pad = b'YnJlYWt3YTExLm1vZQ'
+        input_ = 'breakwa11.moe'
 
-        res_std = SSRProvider.b64decode(standard)
+        # Standard with paddings.
+        standard = base64.urlsafe_b64encode(input_.encode(default_encoding))
+        no_pad = 'YnJlYWt3YTExLm1vZQ'
+
+        res_std = SSRProvider.b64decode(standard.decode(default_encoding))
         res_no_pad = SSRProvider.b64decode(no_pad)
 
         self.assertEqual(input_, res_std)
         self.assertEqual(input_, res_no_pad)
 
-        input_bad = b'YnJlYWt3YTExLm1vZQa'
-        self.assertRaises(ValueError, SSRProvider.b64decode, input_bad)
+    def test_ssr_parse(self):
+        _in = 'ssr://MTI3LjAuMC4xOjEyMzQ6YXV0aF9hZXMxMjhfbWQ1OmFlcy0xMjgtY2ZiOnRsczEuMl90aWNrZXRfYXV0aDpZV0ZoWW1KaS8_b2Jmc3BhcmFtPVluSmxZV3QzWVRFeExtMXZaUQ'
+        _in_remark = 'ssr://MTI3LjAuMC4xOjEyMzQ6YXV0aF9hZXMxMjhfbWQ1OmFlcy0xMjgtY2ZiOnRsczEuMl90aWNrZXRfYXV0aDpZV0ZoWW1KaS8_b2Jmc3BhcmFtPVluSmxZV3QzWVRFeExtMXZaUSZyZW1hcmtzPTVyV0w2Sy1WNUxpdDVwYUg'
+
+        expected = {
+            'server': '127.0.0.1',
+            'server_port': '1234',
+            'password': 'aaabbb',
+            'method': 'aes-128-cfb',
+            'protocol': 'auth_aes128_md5',
+            'obfs': 'tls1.2_ticket_auth'
+        }
+
+        res = SSRProvider.loads(_in)
+        res_remark = SSRProvider.loads(_in_remark)
+
+        for k, v in expected.items():
+            _v = res[k]
+            _vr = res_remark[k]
+            self.assertEqual(v, _v, k)
+            self.assertEqual(v, _vr, '[Remark] %s' % k)
